960639961191600e29bddf57dc73c16b1ff37348
Edward Cerverizzo <edward.cerverizzo@gmail.com>
2021-01-26 14:58:42 -0500

    cleaned up more code. Made control pannnel a class.

--- sketch.js
+++ sketch.js
@@ -10,7 +10,8 @@ function setup() {
 
     numQueens = 8
 
-    setupControlPannel(controlPannelSize, boardSize, screenSize, 8)
+    controlPannel = new ControlPannel()
+    controlPannel.setupControlPannel(controlPannelSize, boardSize, screenSize, 8)
 
     // setup board
     drawBoard(screenSize, boardSize, controlPannelSize)
@@ -31,6 +32,8 @@ function draw() {
     // drawBoard(screenSize, boardSize, controlPannelSize, numberOfQueens)
     // console.log(queensSliderPointer.value())
 
+    drawBoard3(queensSliderPointer.value())
+
     frameRate(5)
     square(x, x, 10)
     x += 10
@@ -42,55 +45,66 @@ function draw() {
 
 }
 
-function setupControlPannel(controlPannelSize, boardSize, screenSize) {
+class ControlPannel {
+    setupControlPannel(controlPannelSize, boardSize, screenSize) {
 
-    let yOffset = (screenSize - boardSize) / 2
+        let yOffset = (screenSize - boardSize) / 2
 
-    let backgroundColor = color(255, 204, 0);
-    fill(backgroundColor)
-    rect(0, yOffset, controlPannelSize, boardSize)
-
-
-    // Control pannel div
-    let controlPannel = createDiv("this is some text")
-    controlPannel.id("controlPannel")
-    controlPannel.style("height", boardSize + "px")
-    controlPannel.style("border", "1px solid black")
-    controlPannel.style("width", controlPannelSize + "px")
-    controlPannel.position(0, 0 + yOffset)
-
-    // controlPannel.parent("main")
+        let backgroundColor = color(255, 204, 0);
+        fill(backgroundColor)
+        rect(0, yOffset, controlPannelSize, boardSize)
 
 
-    // Queens Slider
-    let queensSlider = createSlider(1, 16, 8, 1)
-    queensSlider.id("queensSlider")
-    // queensSlider.position(controlPannelSize * .25, displayHeight * .25)
-    numQueens = queensSlider.value()
+        // Control pannel div
+        let controlPannel = createDiv("this is some text")
+        controlPannel.id("controlPannel")
+        controlPannel.style("height", boardSize + "px")
+        controlPannel.style("border", "1px solid black")
+        controlPannel.style("width", controlPannelSize + "px")
+        controlPannel.position(0, 0 + yOffset)
 
-    // frameRate Slider
-    let frameRateSlider = createSlider(100, 2000, 1000, 100)
-    frameRateSlider.id("frameRateSlider")
-
-    // Start button
-    let startButton = createButton("Start")
-    startButton.mousePressed(beginSolver);
+        // controlPannel.parent("main")
 
 
+        // Queens Slider
+        let queensSlider = createSlider(1, 16, 8, 1)
+        queensSlider.id("queensSlider")
+        // queensSlider.position(controlPannelSize * .25, displayHeight * .25)
+        numQueens = queensSlider.value()
 
-    // Put everything in the [DOM] div element 
-    controlPannel.child(createElement("h3", "Number of Queens"))
-    controlPannel.child(queensSlider)
-    controlPannel.child(createElement("h3", "Frame Rate (speed)"))
-    controlPannel.child(frameRateSlider)
+        // frameRate Slider
+        let frameRateSlider = createSlider(100, 2000, 1000, 100)
+        frameRateSlider.id("frameRateSlider")
 
-    controlPannel.child(startButton)
+        // Start button
+        let startButton = createButton("Start")
+        startButton.id("startButton")
+        startButton.mousePressed(beginSolver);
 
 
 
-    return numQueens
+        // Put everything in the [DOM] div element 
+        controlPannel.child(createElement("h3", "Number of Queens"))
+        controlPannel.child(queensSlider)
+        controlPannel.child(createElement("h3", "Frame Rate (speed)"))
+        controlPannel.child(frameRateSlider)
+
+        controlPannel.child(startButton)
+
+        return numQueens
+
+    }
+
+    disable() {
+        let queensSliderPointer = select("#controlPannel #queensSlider")
+        let startButtonPointer = select("#controlPannel #startButton")
+
+        queensSliderPointer.attribute("disabled", "")    // Disable the 
+        startButtonPointer.attribute("disabled", "")
+    }
 
 }
+
 
 
 function drawBoard(screenSize = screenSize, boardSize = boardSize, controlPannelOffset = controlPannelSize, numberOfQueens = 8) {
@@ -130,7 +144,24 @@ async function drawBoard2(board) {
     }
 }
 
+async function drawBoard3(numberOfQueens) {
 
+    let yOffset = (screenSize - boardSize) / 2
+    // set the drawing point to the top left of the board
+    // translate(controlPannelSize, yOffset)
+
+    let black = color(130)
+    let white = color(25)
+
+    // drawboard
+    for (let i = 0; i < numberOfQueens; i++) {
+        for (let j = 0; j < numberOfQueens; j++) {
+            if ((i + j) % 2 == 0) { fill(black) }
+            else { fill(white) }
+            square((i * (boardSize / numberOfQueens) + controlPannelSize), (j * (boardSize / numberOfQueens)) + yOffset, boardSize / numberOfQueens)
+        }
+    }
+}
 
 
 
@@ -138,11 +169,17 @@ async function drawBoard2(board) {
 // Therefore I created a wrapper function to attach to the start button, if I am looking at this later confused.
 function beginSolver() {
 
+    let queensSliderPointer = select("#controlPannel #queensSlider")
+    noLoop() // disable the draw loop
+
+
+    controlPannel.disable()
+
     // TODO
     // Disable start button to stop two solvers running simultaniously
 
 
-    let s = new Solver()
+    let s = new Solver(queensSliderPointer.value())
     s.solve()
 }
 
@@ -152,13 +189,20 @@ function sleep(ms) {
 
 class Solver {
 
-    constructor() {
-        this.numberOfQueens; // default
-        this.board;
+    constructor(numberOfQueens) {
+        this.numberOfQueens = numberOfQueens;
 
+        this.board = [];
+        for (let i = 0; i < this.numberOfQueens; i++) {
+            this.board.push(0)
+        }
+
+        // colors
         this.checkColor = color(80, 224, 47)
         this.queensColor = color(20, 20, 117)
 
+        // drawing related
+        this.squareSize = (boardSize / this.numberOfQueens)
         this.yOffset = (screenSize - boardSize) / 2
         this.xOffset = controlPannelSize
         this.frameRateSlider = select("#controlPannel #frameRateSlider")
@@ -167,24 +211,12 @@ class Solver {
 
     async solve() {
 
-        // square(300, 150, 20)
-
-        let queensSliderPointer = select("#controlPannel #queensSlider")
-        // let frameRateSlider = select("#controlPannel #frameRateSlider")
-
-        this.numberOfQueens = queensSliderPointer.value()
-        this.board = []
-        for (let i = 0; i < this.numberOfQueens; i++) {
-            this.board.push(0)
-        }
-        // console.log(this.board)
-
         let curr = 0
         while (curr > -1) {
             // console.log(this.board)
 
             await drawBoard2(this.board)
-            await this.drawQueens(this.board, curr)
+            await this.drawQueens(curr)
             await sleep(this.frameRateSlider.value())
             console.log("hello")
 
@@ -215,10 +247,10 @@ class Solver {
         }
     }
 
-    async drawQueens(board, currentQueen) {
+    async drawQueens(currentQueen) {
         fill(this.queensColor)
         for (let i = 0; i < currentQueen + 1; i++) {
-            square(i * (boardSize / this.numberOfQueens) + this.xOffset, board[i] * (boardSize / this.numberOfQueens) + this.yOffset, boardSize / this.numberOfQueens)
+            square(i * this.squareSize + this.xOffset, this.board[i] * this.squareSize + this.yOffset, this.squareSize)
         }
     }
 
@@ -252,12 +284,12 @@ class Solver {
 
         for (let currentCheck = currentQueen - 1; currentCheck > -1; currentCheck--) {
             fill(this.checkColor)
-            square((currentCheck * (boardSize / this.numberOfQueens)) + this.xOffset, (this.board[currentQueen] * (boardSize / this.numberOfQueens)) + this.yOffset, (boardSize / this.numberOfQueens))
+            square((currentCheck * this.squareSize) + this.xOffset, (this.board[currentQueen] * this.squareSize) + this.yOffset, this.squareSize)
             console.log("hello from checkleft")
 
             await sleep(this.frameRateSlider.value())
             await drawBoard2(this.board)
-            await this.drawQueens(this.board, currentQueen)
+            await this.drawQueens(currentQueen)
 
             if (this.board[currentQueen] == this.board[currentCheck]) {
                 return false
@@ -271,11 +303,11 @@ class Solver {
 
         for (let i = 1; currentQueen - i > -1 && this.board[currentQueen] - i > -1; i++) {
             fill(this.checkColor)
-            square((currentQueen - i) * ((boardSize / this.numberOfQueens)) + this.xOffset, ((this.board[currentQueen] - i) * (boardSize / this.numberOfQueens)) + this.yOffset, (boardSize / this.numberOfQueens))
+            square((currentQueen - i) * (this.squareSize) + this.xOffset, ((this.board[currentQueen] - i) * this.squareSize) + this.yOffset, this.squareSize)
 
             await sleep(this.frameRateSlider.value())
             await drawBoard2(this.board)
-            await this.drawQueens(this.board, currentQueen)
+            await this.drawQueens(currentQueen)
 
             if (this.board[currentQueen] - i == this.board[currentQueen - i]) {
                 return false
